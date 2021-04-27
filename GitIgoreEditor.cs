dba945fb957651a9a9e84d8776c1c4ca7636efff
DESKTOP-5B9VU1L\skine <skine134@naver.com>
2021-04-27 12:19:37 +0900

    implement code

--- GitIgoreEditor.cs
+++ GitIgoreEditor.cs
@@ -7,17 +7,28 @@ namespace gitignoreeditor
             var inputStream= System.IO.File.ReadAllText(location);
             var lines = inputStream.Split("\n");
             var writeStream = "";
+            int count = 1;
             foreach(var line in lines)
-            {
-                var writeLine = line;
-                if(!(line.StartsWith("#")||line.StartsWith("*/")||line.StartsWith(".git")||line[0]==13))
+            {   
+                var writeLine="";
+                writeLine+=line;
+                if(!(line.Length<1||line.StartsWith("#")||line.StartsWith("*/")||line.StartsWith(".git")||line[0]==13))
                 {
                     writeLine = $"*/{writeLine}";
                 }
-                writeStream+=$"{writeLine}\n";
+                writeStream+=$"{writeLine}";
+                if(count!=lines.Length)
+                    writeLine += $"\n";
+                ++count;
+            }
+            if(inputStream.Equals(writeStream))
+            {
+                System.Console.WriteLine("file is not changed");
+                return;
             }
             System.IO.File.WriteAllText($"{location}_temp",inputStream);
             System.IO.File.WriteAllText(location,writeStream);
+            System.Console.WriteLine($"Save file : {location}");
         }
     }
 }
